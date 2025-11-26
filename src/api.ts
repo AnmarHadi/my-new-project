dd7af83ccda0022b53740d637fcbfe8dfe8495f8
AnmarHadi <anmarhadi76@gmail.com>
2025-11-26 22:21:27 +0300

    fix: route API calls to Netlify Functions

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 // في التطوير: Vite proxy سيحوّل /api إلى http://localhost:5000
 // في الإنتاج: استخدم نفس الأصل (القيمة النسبية تعمل مع السيرفر الذي يقدّم الواجهة)
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "/.netlify/functions"
 });
 
 export default api;
